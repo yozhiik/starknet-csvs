a3f26afc0cdc8ffd0126ec9860a599fdaf8c7fbc
yozhiik <84688089+yozhiik@users.noreply.github.com>
2024-01-30 14:23:22 +0000

    added transfers download option

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# starknet-csvs
-# starknet-csvs
+## Description
+This is a very basic python tool to download transaction and transfer history from Voyager, written in python 3 by me. This is literally my first day writing a python script so be nice
+
+### Setup
+just run:
+python export-starknet.py [wallet address] [transactions / transfers] output.csv
--- export-starknet.py
+++ export-starknet.py
@@ -5,35 +5,65 @@ from datetime import datetime
 import argparse
 parser=argparse.ArgumentParser(description="add numbers")
 parser.add_argument("wallet_address", type=str)
+parser.add_argument("download_type", type=str)
 parser.add_argument("filename", type=str)
 args=parser.parse_args()
 wallet_address=args.wallet_address
+download_type=args.download_type
 f_name=args.filename
-
-base_url = "https://voyager.online/api/txns"
 per_page = 100
+max_pages = 100000
 
-# pull in data from API
+# init 
 page = 1
-payload = {'to': wallet_address, 'ps': per_page, 'p': page, 'type': "null"}
-json_page = requests.get(base_url, params=payload).json()
-num_pages = json_page["lastPage"]
-page_data = json_page["items"]
 
-# random strings passed as the address lead to the entire blockchain's index being passed (currently 900,000 ish) so don't save it all
-if num_pages == 0 or num_pages > 100000:
-    print("address not found, double check on voyager")
+if download_type != "transfers" and download_type != "transactions":
+    print("download type must be 'transactions' or 'transfers'")
     sys.exit()
 
-## loop through and add the rest of the items from the other pages
-for i in range(2, num_pages + 1):
-    payload = {'to': wallet_address, 'ps': per_page, 'p': i, 'type': "null"}
+if download_type == 'transactions':
+    base_url = "https://voyager.online/api/txns"
+    payload = {'to': wallet_address, 'ps': per_page, 'p': page, 'type': "null"}
+
     json_page = requests.get(base_url, params=payload).json()
-    # append this page data to the first page's
-    page_data = page_data + json_page["items"]
+    num_pages = json_page["lastPage"]
+    page_data = json_page["items"]
 
+    # random strings passed as the address lead to the entire blockchain's index being passed (currently 900,000 ish) so don't save it all
+    if num_pages == 0 or num_pages > max_pages:
+        print("address not found, double check on voyager")
+        sys.exit()
 
-fields = ["utcTime", "blockId", "blockNumber", "hash", "index", "l1VerificationHash", "type", "class_hash", "sender_address", "contract_address", "timestamp", "actual_fee", "execution_status", "revert_error", "domain", "status", "finality_status", "operations", "classAlias", "contractAlias", "senderAlias"]
+    ## loop through and add the rest of the items from the other pages
+    for i in range(2, num_pages + 1):
+        payload = {'to': wallet_address, 'ps': per_page, 'p': i, 'type': "null"}
+        json_page = requests.get(base_url, params=payload).json()
+        # append this page data to the first page's
+        page_data = page_data + json_page["items"]
+
+    fields = ["utcTime", "blockId", "blockNumber", "hash", "index", "l1VerificationHash", "type", "class_hash", "sender_address", "contract_address", "timestamp", "actual_fee", "execution_status", "revert_error", "domain", "status", "finality_status", "operations", "classAlias", "contractAlias", "senderAlias"]
+else:
+    # this is transfers
+    base_url = "https://voyager.online/api/contract/" + wallet_address + "/transfers"
+    payload = {'ps': per_page, 'p': page}
+
+    json_page = requests.get(base_url, params=payload).json()
+    num_pages = json_page["lastPage"]
+    page_data = json_page["items"]
+
+    # random strings passed as the address lead to the entire blockchain's index being passed (currently 900,000 ish) so don't save it all
+    if num_pages == 0 or num_pages > max_pages:
+        print("address not found, double check on voyager")
+        sys.exit()
+
+    ## loop through and add the rest of the items from the other pages
+    for i in range(2, num_pages + 1):
+        payload = {'ps': per_page, 'p': i}
+        json_page = requests.get(base_url, params=payload).json()
+        # append this page data to the first page's
+        page_data = page_data + json_page["items"]
+
+    fields = ["utcTime","blockNumber", "tokenAddress", "timestamp", "transferFrom", "transferTo", "transferValue", "txHash", "callName", "tokenName", "tokenSymbol", "tokenDecimals", "blockHash", "fromAlias", "toAlias"]
 
 # sanitise / tidy up - set blank entries if missing values, replace new lines
 for row in page_data:
