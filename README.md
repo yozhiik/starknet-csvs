81ab47c94b835febe3beeffe050a6dda394ee02a
yozhiik <84688089+yozhiik@users.noreply.github.com>
2024-02-06 21:59:32 +0000

    v0.2.0

--- README.md
+++ README.md
@@ -1,8 +1,24 @@
-## Description
-This is a very basic python tool to download transaction and transfer history from Voyager, written in python 3 by me. This is literally my first day writing a python script so be nice. It outputs the timestamp in UTC, but I haven't messed with values in WEI for e.g. fees, since fees may later be paid in $STRK. You can use Excel or whatever to massage it into the format you like for Koinly / whatever tax software you use.
+# Description
+This is a very basic python tool to download transaction and transfer history from Voyager, written in python 3 by me. This is literally my first day writing a python script so be nice. 
+To use it, you'll just need the (free!) API key. Please fill out the form here requesting one and someone from Voyager will get back to you sharpish: https://forms.gle/34RE6d4aiiv16HoW6
+Voyager docs, for reference, are here: https://docs.voyager.online/#overview
 
-Caveat: transactions / transfers before around Nov 2022 may not be fully recorded anywhere that I can find!
+## Features: 
+    - outputs the timestamp in UTC
+    - converts eth transactions / fees to eth (from wei). 
+    - allows a .stark domain as input
+    - automatically formats output csv filenames using the wallet name you provide, with the current date and time, without needing this as a param any more
+    - shows, for transfers, if they're going into or coming out of your account
+    - works with the official API!
 
-### Setup
+## Caveats:
+    - transactions / transfers before around Nov 2022 may not be fully recorded anywhere that I can find, since emissions for events weren't working at that point (I think)
+    - As fees end up paid with non-eth currencies like $strk when they start being used. The explorers' APIs will likely change, so need to keep an eye on this!
+
+# Setup
 just run:
-`python export-starknet.py [wallet address] [transactions / transfers] output.csv`
+`python export-starknet.py [wallet address] [transactions / transfers] api_key`
+e.g. 
+`python export-starknet.py test.stark transfers xyx0123fdsjfldskj`
+will generate 
+transfers_paper.stark_2024-02-06 21:26:00.552429.csv
--- export-starknet.py
+++ export-starknet.py
@@ -1,81 +1,129 @@
 # import starknet transactions from Voyager
-import sys, argparse, requests, json, csv 
+import sys, argparse, requests, json, csv, configparser, re
 from datetime import datetime
 
-import argparse
+def get_stark_domain(domain):
+    try:
+        json_page = requests.get("https://api.starknet.id/domain_to_addr?domain=" + domain).json()
+        if "addr" in json_page:
+            return json_page["addr"]
+    except:
+        raise Exception("no address found for "+domain+" using starknet api")
+
+def check_api_valid(page_data):
+    if "message" in page_data:
+        raise Exception('Voyager API not valid, error returned "'+page_data['message']+'".\nRequest a free API key from the team here: https://forms.gle/34RE6d4aiiv16HoW6\nDocs here: https://docs.voyager.online/#overview')
+
+def convert_wei_to_eth(wei_val):
+    return float(int(wei_val)*0.000000000000000001)
+
+def get_transactions_data():
+    transactions_url = base_url + "/txns"
+    payload = {'ps': per_page, 'p': 1}
+    payload['to'] = wallet_address 
+    json_page = requests.get(transactions_url, headers=headers, params=payload).json()
+    check_api_valid(json_page) ## check API key is working
+    num_pages = json_page["lastPage"]
+    page_data = json_page["items"]
+    # random strings passed as the address lead to the entire blockchain's index being passed (currently 900,000 ish) so don't save it all
+    if num_pages == 0 or num_pages > max_pages:
+        raise Exception("address not found, double check on voyager")
+    
+    ## loop through and add the rest of the items from the other pages
+    for i in range(2, num_pages + 1):
+        payload['p'] = i # update page number
+        json_page = requests.get(transactions_url, headers=headers, params=payload).json()
+        # append this page data to the first page's
+        page_data = page_data + json_page["items"]
+    return page_data
+
+def get_transfers_data(type):
+    transfers_url = base_url + "/token-transfers"
+    payload = {'page_size': per_page, 'page': 1}
+    payload[type] = wallet_address
+    json_page = requests.get(transfers_url, headers=headers, params=payload).json()
+    check_api_valid(json_page) ## check API key is working, die if not
+    num_pages = json_page["lastPage"]
+    page_data = json_page["items"]
+    # random strings passed as the address may lead to the entire blockchain's index being passed (currently 900,000 ish) so don't save it all
+    if num_pages == 0 or num_pages > max_pages:
+        return # just leave blank
+    # loop through and add the rest of the items from the other pages
+    for i in range(2, num_pages + 1):
+        payload['page'] = i
+        json_page = requests.get(transfers_url, headers=headers, params=payload).json()
+        # append this page data to the first page's
+        page_data = page_data + json_page["items"]
+    return page_data
+
+def process_fields(page_data, fields):
+    # sanitize / tidy up - set blank entries if missing values, replace new lines
+    for row in page_data:
+        for field in fields:
+            if field == "utcTime":
+                row[field] = datetime.utcfromtimestamp(row["timestamp"])
+            if field == "in_or_out":
+                if row["from_address"] == wallet_address:
+                    row[field] = "OUT"
+                elif row["to_address"] == wallet_address:
+                    row[field] = "IN"
+            if convert_wei and field == "actualFee":
+                row[field] = convert_wei_to_eth(row[field])
+            if field == "transfer_amounts":
+                if len(row[field]) > 1:
+                    raise Exception("transfer_amounts has more than one value")
+                row[field] = (row[field][0])
+                if convert_wei:
+                    if row["token_address"] == eth_contract: # only convert for the eth contract
+                        row[field] = convert_wei_to_eth(row[field])
+            if not field in row:
+                row[field] = ""
+            if isinstance(row[field], str):
+                row[field] = row[field].replace("\n", "\\n")
+            
+
+# init, globals
+page = 1
+per_page = 100
+max_pages = 1000
+base_url = "https://api.voyager.online/beta"
+convert_wei = True
+eth_contract = "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7"
+
+#cli args input
 parser=argparse.ArgumentParser(description="add numbers")
 parser.add_argument("wallet_address", type=str)
 parser.add_argument("download_type", type=str)
-parser.add_argument("filename", type=str)
+parser.add_argument("api_key", type=str)
 args=parser.parse_args()
-wallet_address=args.wallet_address
+fname_address = args.wallet_address
+wallet_address = args.wallet_address
+if re.search("stark", wallet_address):
+    wallet_address = get_stark_domain(wallet_address)
 download_type=args.download_type
-f_name=args.filename
-per_page = 100
-max_pages = 100000
-
-# init 
-page = 1
+f_name = download_type + "_" + fname_address + "_" + str(datetime.utcnow()) + ".csv"
+api_key = args.api_key
+headers = {
+    'Accept': 'application/json',
+    'X-Api-Key': api_key
+}
 
 if download_type != "transfers" and download_type != "transactions":
-    print("download type must be 'transactions' or 'transfers'")
-    sys.exit()
+    raise Exception("download type must be 'transactions' or 'transfers'")
 
 if download_type == 'transactions':
-    base_url = "https://voyager.online/api/txns"
-    payload = {'to': wallet_address, 'ps': per_page, 'p': page, 'type': "null"}
-
-    json_page = requests.get(base_url, params=payload).json()
-    num_pages = json_page["lastPage"]
-    page_data = json_page["items"]
-
-    # random strings passed as the address lead to the entire blockchain's index being passed (currently 900,000 ish) so don't save it all
-    if num_pages == 0 or num_pages > max_pages:
-        print("address not found, double check on voyager")
-        sys.exit()
-
-    ## loop through and add the rest of the items from the other pages
-    for i in range(2, num_pages + 1):
-        payload = {'to': wallet_address, 'ps': per_page, 'p': i, 'type': "null"}
-        json_page = requests.get(base_url, params=payload).json()
-        # append this page data to the first page's
-        page_data = page_data + json_page["items"]
-
-    fields = ["utcTime", "blockId", "blockNumber", "hash", "index", "l1VerificationHash", "type", "class_hash", "sender_address", "contract_address", "timestamp", "actual_fee", "execution_status", "revert_error", "domain", "status", "finality_status", "operations", "classAlias", "contractAlias", "senderAlias"]
+    page_data = get_transactions_data()
+    fields = ["utcTime","status","type","blockNumber","hash","index","l1VerificationHash","classHash","contractAddress","timestamp","actualFee","actions","contractAlias","classAlias"]
 else:
     # this is transfers
-    base_url = "https://voyager.online/api/contract/" + wallet_address + "/transfers"
-    payload = {'ps': per_page, 'p': page}
+    to_page_data = get_transfers_data("to_address")
+    from_page_data = get_transfers_data("from_address")
+    page_data = to_page_data + from_page_data
+    # sort by timestamp
+    page_data.sort(key = lambda x: x['timestamp'])
+    fields = ["utcTime", "block_number", "token_address", "transaction_hash", "event_selector", "event_index", "timestamp", "invocation_type", "token_type", "from_address", "to_address", "data_len", "transfer_amounts", "in_or_out"]
 
-    json_page = requests.get(base_url, params=payload).json()
-    num_pages = json_page["lastPage"]
-    page_data = json_page["items"]
-
-    # random strings passed as the address lead to the entire blockchain's index being passed (currently 900,000 ish) so don't save it all
-    if num_pages == 0 or num_pages > max_pages:
-        print("address not found, double check on voyager")
-        sys.exit()
-
-    ## loop through and add the rest of the items from the other pages
-    for i in range(2, num_pages + 1):
-        payload = {'ps': per_page, 'p': i}
-        json_page = requests.get(base_url, params=payload).json()
-        # append this page data to the first page's
-        page_data = page_data + json_page["items"]
-
-    fields = ["utcTime","blockNumber", "tokenAddress", "timestamp", "transferFrom", "transferTo", "transferValue", "txHash", "callName", "tokenName", "tokenSymbol", "tokenDecimals", "blockHash", "fromAlias", "toAlias"]
-
-# sanitise / tidy up - set blank entries if missing values, replace new lines
-for row in page_data:
-    for field in fields:
-        if field == "utcTime":
-            row[field] = datetime.utcfromtimestamp(row["timestamp"])
-        if not field in row:
-            row[field] = ""
-        if isinstance(row[field], str):
-            row[field] = row[field].replace("\n", "\\n")
-        
-
+process_fields(page_data, fields)
 with open(f_name, 'w') as csv_file:
     writer = csv.writer(csv_file)
     writer.writerow(fields) # header
